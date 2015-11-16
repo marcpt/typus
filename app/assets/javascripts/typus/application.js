697f99581e4295f44460ed4545b1c8ea5c1959bb
Mario <mcardoso@imaginarycloud.com>
2015-11-16 17:23:17 +0000

    Make modals in assotiation tables usable

--- app/assets/javascripts/typus/application.js
+++ app/assets/javascripts/typus/application.js
@@ -8,4 +8,12 @@ $(document).on('click', '.ajax-modal', function() {
   var url = $(this).attr('url');
   var modal_id = $(this).attr('data-controls-modal');
   $("#" + modal_id + " .modal-body").load(url);
+
+  var action = $(this).attr('glyphicon');
+  // change footer
+  if(action == "info-sign"){
+    $("#" + modal_id + " .modal-footer").hide();
+  } else {
+    $("#" + modal_id + " .modal-footer").show();
+  }
 });
